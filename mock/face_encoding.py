c98932025c0ff73b34e5a3ed06ebeacbcc5a1c65
罗策 <ce.luo@yeadun.com>
2019-04-03 13:37:52 +0800

    add img

--- mock/face_encoding.py
+++ mock/face_encoding.py
@@ -3,5 +3,8 @@
 
 import face_recognition
 
-print face_recognition.face_encodings(face_recognition.load_image_file(u'/Users/huan/sources/python-sources/face-recognition-service/face_data/know_face_dir/王思聪.jpeg'))
-print face_recognition.face_encodings(face_recognition.load_image_file(u'/Users/huan/sources/python-sources/face-recognition-service/face_data/know_face_dir/林狗.jpeg'))
+# print face_recognition.face_encodings(face_recognition.load_image_file(u'/Users/huan/sources/python-sources/face-recognition-service/face_data/know_face_dir/王思聪.jpeg'))
+# print face_recognition.face_encodings(face_recognition.load_image_file(u'/Users/huan/sources/python-sources/face-recognition-service/face_data/know_face_dir/林狗.jpeg'))
+
+print face_recognition.face_encodings(face_recognition.load_image_file(u'/Users/huan/sources/python-sources/face-recognition-service/face_data/know_face_dir/张曼.jpg'))
+print face_recognition.face_encodings(face_recognition.load_image_file(u'/Users/huan/sources/python-sources/face-recognition-service/face_data/know_face_dir/宋克军.JPG'))
